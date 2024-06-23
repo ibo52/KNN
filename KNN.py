352c9ea6e968ffc407ac0ef25d7a1c99d229d753
ibrahim mut <halilibrahimmut@hotmaiil.com>
2024-06-23 13:17:27 +0300

    Basic KNN implementation for color detection has implemented

--- KNN.py
+++ KNN.py
@@ -2,19 +2,71 @@ import os
 import math
 import operator
 import csv
+import cv2
+import numpy as np
+from imageDataLoader import ImageDataLoader
+from threading import Thread
 
 class KNN:
-    def __init__(self, n_neighbors=5, weights="distance"):
+    def __init__(self, train_dataPath="C:\\Users\\ibrahim mut\\Desktop\\KNN_color_recognition-master\\training_dataset", n_neighbors=5, weights="distance"):
+
         self.feature_map=[]
-        pass
 
-    def fit(self, X, Y):
-        pass
+        self.dataGenerator=ImageDataLoader(train_dataPath)
+
+    def fit(self, X, verbose=1):
+
+        colors=("b", "g", "r")#cv2 reads as b,g,r
+
+        iteration=0
+        for img, belongedClass in X:
+
+            channels=cv2.split(img)
+
+            feature=[]
+            for nth_channel, nth_color in zip(channels, colors):
+
+                hist=cv2.calcHist(nth_channel, [0], None, [256], [0,256])
+
+                # find the peak pixel values for R, G, and B
+                feature.append( np.argmax(hist) )
 
 
-    def predict(self, arr, n_neighbors=3):
-        n=self.findKNearestNeighbors(self.feature_map, arr, n_neighbors)
-        return self.determineClass(n)
+            #---VERBOSE PROCESS----------------------
+            if verbose>0:
+                WIDTH_CONSTANT=70#constant may be max width of terminal
+                ratio=int(iteration*WIDTH_CONSTANT/self.dataGenerator.generatorLen)
+
+                #carriage return is may be windows specific
+                print(f"\rTraining Process: [",end="")
+                print("*"*ratio, end="")
+                print(" "*int(WIDTH_CONSTANT-ratio-1), end="")
+                print("]", end="")
+                iteration+=1
+            #---VERBOSE PROCESS----------------------
+
+            with open("model.csv", "a") as f:
+                #save found r,g,b feature of belonging class
+                f.write(f"{feature[2]}, {feature[1]}, {feature[0]}, {belongedClass}\n")
+
+        print(" Done")
+
+    def predict(self, imageArray, n_neighbors=3):
+
+        channels=cv2.split(imageArray)
+
+        feature=[]
+        for nth_channel, nth_color in zip(channels, ("b", "g", "r")):
+
+            hist=cv2.calcHist(nth_channel, [0], None, [256], [0,256])
+            # find the peak pixel values for R, G, and B
+            feature.append( np.argmax(hist) )
+
+
+        n=self.findKNearestNeighbors(self.feature_map, feature, n_neighbors)
+        scores= self.determineClass(n)
+        #print("scores:",scores)
+        return max(scores, key= lambda x: x[1])[0]
 
     def calculateEuclideanDistance(self, array1=[], array2=[], arrayDimensions=0):
         dist=0.0
@@ -25,20 +77,43 @@ class KNN:
         return math.sqrt(dist)
 
     def findKNearestNeighbors(self, known_map, input_sample, n_neighbors=1):
-        dists=[]
 
-        for _ in range(len(known_map)):
 
-            d=self.calculateEuclideanDistance(input_sample, known_map[_], len(input_sample))
-            dists.append( (known_map[_], d) )#keep distances to every data points of given input_sample
+        NUM_THREADS=os.cpu_count()
+        map_size=len(known_map)
 
-            dists.sort(key=operator.itemgetter(1))#sort by distances
+        threads=[]
+        results=[]
 
-        #return k nearest neighbors
-        if 0 < n_neighbors <= len(dists):
-            return dists[:n_neighbors]
-        else:
-            return dists
+        for index in range(0, map_size, NUM_THREADS):
+
+            threadRange=(index, max(index+NUM_THREADS, map_size))
+
+            t=Thread(target=self.__findKNearestNeighborsThread, args=(known_map, input_sample, threadRange, results))
+            t.start()
+            threads.append( t )
+
+        for thread in threads:
+            thread.join()
+
+        results.sort(key=operator.itemgetter(1))#sort by distances
+        results= results[:n_neighbors] if 0<n_neighbors<=len(results) else results
+
+        return results
+
+    def __findKNearestNeighborsThread(self, known_map, input_sample, threadRange:tuple=(), result:list=[]):
+        dists=[]#distances
+
+        for nth_feature in known_map[threadRange[0]: threadRange[1]]:
+
+            d=self.calculateEuclideanDistance(input_sample, nth_feature, len(input_sample))
+            dists.append( (nth_feature, d) )#keep distances to every data points of given input_sample
+
+        dists.sort(key=operator.itemgetter(1))#sort by distances
+
+        #return neighbors
+        for neighbor in dists:
+            result.append( neighbor )
 
     def determineClass(self, neighbors):
         classes={}
@@ -73,8 +148,14 @@ class KNN:
             retval=1
         else:
             print ('No file(or access permissions) for KNN model is found!:',path)
+            print("Please provide or train a model. Exiting..")&exit(-1)
 
         return retval
-k=KNN()
-k.loadModel("training.data")
-print([255,128,243]," belongs to class;",k.predict([255,128,243], 5))
+
+if __name__=="__main__":
+    k=KNN()
+    k.fit(k.dataGenerator.generator)#train
+    k.loadModel("model.csv")
+
+    img=cv2.imread("C:\\Users\\ibrahim mut\\Desktop\\KNN_color_recognition-master\\training_dataset\\green\\green7.png")
+    print("image belongs to class;",k.predict(img, 5))
